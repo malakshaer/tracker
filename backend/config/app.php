4a0fad2c2133225ddba9da4177d3356919c6a60e
malakshaer <shaermk06@gmail.com>
2022-10-25 17:00:19 +0300

    backend: firebase service provider added

--- backend/config/app.php
+++ backend/config/app.php
@@ -182,6 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Kreait\Laravel\Firebase\ServiceProvider::class,
 
         /*
          * Package Service Providers...
