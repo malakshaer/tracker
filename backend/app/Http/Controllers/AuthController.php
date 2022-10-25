cb6ea1342f0edf47cd12bc327e5456591aed37e5
malakshaer <shaermk06@gmail.com>
2022-10-25 17:05:00 +0300

    backend: linking firebase with project completed

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,26 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login']]);
     }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function login()
     {
         $credentials = request(['email', 'password']);
@@ -33,11 +26,28 @@ class AuthController extends Controller
         return $this->respondWithToken($token);
     }
 
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
+    public function register(Request $request)
+    {
+        $confirmation_code = String::random(15);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'confirmation_code' => $confirmation_code,
+            'profile_img' => $request->profile_img
+        ]);
+
+        // Mail::send('auth.emails.user-confirmation', ['name' => $name, 'confirmation_code' => $user['confirmation_code']], function ($message) use ($email, $name) {
+        //     $message->from('mymail@gmail.com', 'Name Family')->to($email, $name)->subject('Confirm email');
+        // });
+
+        return response()->json([
+            "status" => "success",
+            "data" => "User successfully created"
+        ]);
+    }
+
     public function me()
     {
         return response()->json(auth()->user());
