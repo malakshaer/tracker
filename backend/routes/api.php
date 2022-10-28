54afc7ae8aae4a8ef34edb10c8ba7f1133a88831
malakshaer <shaermk06@gmail.com>
2022-10-28 11:12:13 +0300

    backend: notification controller and car controller routes are added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,4 +20,23 @@ Route::group(["prefix" => "auth"], function () {
         Route::put('/editUser/{data}', 'editUser');
         Route::delete('/deleteAccount/{id}', 'deleteAccount');
     });
+
+    Route::controller(NotificationController::class)->group(function () {
+        Route::post('/sendNotification', 'sendNotification');
+        Route::get('/getAllNotifications', 'getAllNotifications');
+        Route::get('/getSingleNotification/{id}', 'getSingleNotification');
+        Route::put('/readNotification/{id}', 'readNotification');
+        Route::delete('/deleteNotification/{id}', 'deleteNotification');
+        Route::delete('/deleteAllNotifications', 'deleteAllNotifications');
+    });
+
+    Route::controller(CarController::class)->group(function () {
+        Route::post('/createNewCar', 'createNewCar');
+        Route::get('/getAllCars', 'getAllCars');
+        Route::get('/getCar/{id}', 'getCar');
+        Route::put('/editCar/{id}', 'editCar');
+        Route::put('/changeCarStatus/{id}', 'changeCarStatus'); //
+        Route::delete('/deleteAllCars', 'deleteAllCars');
+        Route::delete('/deleteCar/{id}', 'deleteCar');
+    });
 });
