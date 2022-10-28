67b800f071e3230395d9a7eafcdf7f63f89632fb
malakshaer <shaermk06@gmail.com>
2022-10-28 11:50:31 +0300

    backend: middleware is added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,29 +14,31 @@ Route::group(["prefix" => "auth"], function () {
         Route::post("/login", 'login');
         Route::post("/register", 'register');
     });
+    Route::middleware(['auth:api'])->group(function () {
 
-    Route::controller(UserController::class)->group(function () {
-        Route::get('/showProfile/{id}', 'showProfile');
-        Route::put('/editUser/{data}', 'editUser');
-        Route::delete('/deleteAccount/{id}', 'deleteAccount');
-    });
+        Route::controller(UserController::class)->group(function () {
+            Route::get('/showProfile/{id}', 'showProfile');
+            Route::put('/editUser/{data}', 'editUser');
+            Route::delete('/deleteAccount/{id}', 'deleteAccount');
+        });
 
-    Route::controller(NotificationController::class)->group(function () {
-        Route::post('/sendNotification', 'sendNotification');
-        Route::get('/getAllNotifications', 'getAllNotifications');
-        Route::get('/getSingleNotification/{id}', 'getSingleNotification');
-        Route::put('/readNotification/{id}', 'readNotification');
-        Route::delete('/deleteNotification/{id}', 'deleteNotification');
-        Route::delete('/deleteAllNotifications', 'deleteAllNotifications');
-    });
+        Route::controller(NotificationController::class)->group(function () {
+            Route::post('/sendNotification', 'sendNotification');
+            Route::get('/getAllNotifications', 'getAllNotifications');
+            Route::get('/getSingleNotification/{id}', 'getSingleNotification');
+            Route::put('/readNotification/{id}', 'readNotification');
+            Route::delete('/deleteNotification/{id}', 'deleteNotification');
+            Route::delete('/deleteAllNotifications', 'deleteAllNotifications');
+        });
 
-    Route::controller(CarController::class)->group(function () {
-        Route::post('/createNewCar', 'createNewCar');
-        Route::get('/getAllCars', 'getAllCars');
-        Route::get('/getCar/{id}', 'getCar');
-        Route::put('/editCar/{id}', 'editCar');
-        Route::put('/changeCarStatus/{id}', 'changeCarStatus'); //
-        Route::delete('/deleteAllCars', 'deleteAllCars');
-        Route::delete('/deleteCar/{id}', 'deleteCar');
+        Route::controller(CarController::class)->group(function () {
+            Route::post('/createNewCar', 'createNewCar');
+            Route::get('/getAllCars', 'getAllCars');
+            Route::get('/getCar/{id}', 'getCar');
+            Route::put('/editCar/{id}', 'editCar');
+            Route::put('/changeCarStatus/{id}', 'changeCarStatus'); //
+            Route::delete('/deleteAllCars', 'deleteAllCars');
+            Route::delete('/deleteCar/{id}', 'deleteCar');
+        });
     });
 });
