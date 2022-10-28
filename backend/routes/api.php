aaf2a4a015a7ad536d521a1267a0254293e7e9aa
malakshaer <shaermk06@gmail.com>
2022-10-28 11:11:23 +0300

    backend: auth controller and user controller routes are added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "auth"], function () {
+
+    Route::controller(AuthController::class)->group(function () {
+        Route::post("/login", 'login');
+        Route::post("/register", 'register');
+    });
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/showProfile/{id}', 'showProfile');
+        Route::put('/editUser/{data}', 'editUser');
+        Route::delete('/deleteAccount/{id}', 'deleteAccount');
+    });
 });
