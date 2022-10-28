9b68f3183544e432dcf53dec061cbe4cc10e64cc
malakshaer <shaermk06@gmail.com>
2022-10-28 10:40:41 +0300

    backend: firebase user provider added

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -2,29 +2,23 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
+use App\Firebase\Guard as FirebaseGuard;
+use App\Firebase\FirebaseUserProvider;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->registerPolicies();
 
-        //
+        \Illuminate\Support\Facades\Auth::provider('firebaseuserprovider', function ($app, array $config) {
+            return new FirebaseUserProvider($app['hash'], $config['model']);
+        });
     }
 }
