52827d9ab6fdf6a1d32402cf50ceca9afd9e58f2
malakshaer <shaermk06@gmail.com>
2022-11-16 08:36:26 +0200

    frontend: add the styles of the header

--- frontend/src/navigation/OnboardingStack.jsx
+++ frontend/src/navigation/OnboardingStack.jsx
@@ -6,14 +6,27 @@ import LandingScreen from "../screens/LandingScreen/LandingScreen";
 import LogInScreen from "../screens/LogInScreen/LogInScreen";
 import RegisterScreen from "../screens/RegisterScreen/RegisterScreen";
 import TabStack from "./TabStack";
+// import MapScreen from "../screens/AppScreens/MapScreen/MapScreen";
+// import { colors } from "../constants/palette";
 
 export function OnboardingStack() {
   const Stack = createStackNavigator();
   const navigationRef = useRef(null);
 
   return (
-    <NavigationContainer ref={navigationRef}>
-      <Stack.Navigator>
+    <NavigationContainer ref={navigationRef} independent={true}>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#032955",
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontSize: 24,
+          },
+        }}
+      >
         <Stack.Screen name="Tracker" component={LandingScreen} />
         <Stack.Screen
           name="Login"
