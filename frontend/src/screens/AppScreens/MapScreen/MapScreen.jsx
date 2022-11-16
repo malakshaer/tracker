4f6be51f4eec51e2f8d308cdee45895d7661cfc5
malakshaer <shaermk06@gmail.com>
2022-11-16 07:31:10 +0200

    frontend: add the styles of the screen

--- frontend/src/screens/AppScreens/MapScreen/MapScreen.jsx
+++ frontend/src/screens/AppScreens/MapScreen/MapScreen.jsx
@@ -80,4 +80,26 @@ const MapScreen = () => {
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bottomCard: {
+    backgroundColor: "white",
+    width: "100%",
+    padding: 30,
+    borderTopEndRadius: 24,
+    borderTopStartRadius: 24,
+  },
+  inputStyle: {
+    backgroundColor: "white",
+    borderRadius: 4,
+    borderWidth: 1,
+    alignItems: "center",
+    height: 48,
+    justifyContent: "center",
+    marginTop: 16,
+  },
+});
+
 export default MapScreen;
