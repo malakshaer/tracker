83a529c913c6fa3ec8898be9ace89b19a95f0a62
malakshaer <shaermk06@gmail.com>
2022-11-16 08:54:39 +0200

    frontend: add the navigation container

---
+++ frontend/App.jsx
@@ -0,0 +1,43 @@
+import "react-native-gesture-handler";
+import { createContext, useState } from "react";
+import TabStack from "./src/navigation/TabStack";
+import { OnboardingStack } from "./src/navigation/OnboardingStack";
+import { NavigationContainer } from "@react-navigation/native";
+import { StackSwitcher } from "./src/navigation/StackSwitcher";
+// import { AuthProvider } from "./src/redux/AuthContext";
+
+// const ContextProvider = createContext();
+
+export const UserContext = createContext("");
+
+const App = () => {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [token, setToken] = useState("");
+  return (
+    <TabStack />
+    // <OnboardingStack />
+    // <>
+    //   <UserContext.Provider
+    //     value={{
+    //       id,
+    //       setId,
+    //       token,
+    //       setToken,
+    //       name,
+    //       setName,
+    //       email,
+    //       setEmail,
+    //     }}
+    //   >
+    //     <NavigationContainer>
+    //       <OnboardingStack />
+    //     </NavigationContainer>
+    //   </UserContext.Provider>
+    // </>
+  );
+};
+
+export default App;
