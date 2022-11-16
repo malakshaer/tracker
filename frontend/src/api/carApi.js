757f9028ee7df5b18c07894e9427fb618382f06e
malakshaer <shaermk06@gmail.com>
2022-11-16 08:02:58 +0200

    frontend: call the get car api

---
+++ frontend/src/api/carApi.js
@@ -0,0 +1,29 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+const BASE_URL = "http://127.0.0.1:8000/api/auth";
+
+export const getAllCars = async () => {
+  const user = JSON.parse((await AsyncStorage.getItem("user")) || "");
+  console.log("data");
+
+  const res = await axios.get(`${BASE_URL}/getAllCars`, {
+    headers: {
+      Authorization: `Bearer ${user?.access_token}`,
+    },
+  });
+
+  return res;
+};
+
+export const getCar = async (data) => {
+  const user = JSON.parse((await AsyncStorage.getItem("user")) || "");
+  console.log("data");
+
+  const res = await axios.get(`${BASE_URL}/getCar/${data}`, {
+    headers: {
+      Authorization: `Bearer ${user?.access_token}`,
+    },
+  });
+
+  return res;
+};
