4393fb85f6520703d740824f49682cedd77beeb4
malakshaer <shaermk06@gmail.com>
2022-11-16 08:21:44 +0200

    frontend: add the toggle modal function

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -2,6 +2,31 @@ import React, { useEffect, useState } from "react";
 import { View, Modal, StyleSheet, Animated } from "react-native";
 
 const ModalPopup = ({ visible, children }) => {
+  const [showModal, setShowModal] = React.useState(visible);
+  const scaleValue = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    toggleModal();
+  }, [visible]);
+
+  const toggleModal = () => {
+    if (visible) {
+      setShowModal(true);
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      setTimeout(() => setShowModal(false), 200);
+      Animated.timing(scaleValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+
   return (
     <Modal transparent visible={showModal}>
       <View style={styles.modalBackGround}>
