14ac4ce2c3cf73322dfd1fe8198778685f9c7135
malakshaer <shaermk06@gmail.com>
2022-11-16 07:36:06 +0200

    frontend: add the permission function to get location

--- frontend/src/screens/AppScreens/MapScreen/helperFunction.js
+++ frontend/src/screens/AppScreens/MapScreen/helperFunction.js
@@ -20,3 +20,32 @@ export const getCurrentLocation = () =>
       { enableHighAccuracy: true, timeout: 900000000, maximumAge: 10000 }
     );
   });
+export const locationPermission = () =>
+  new Promise(async (resolve, reject) => {
+    if (Platform.OS === "ios") {
+      try {
+        const permissionStatus = await Geolocation.requestAuthorization(
+          "whenInUse"
+        );
+        if (permissionStatus === "granted") {
+          return resolve("granted");
+        }
+        reject("Permission not granted");
+      } catch (error) {
+        return reject(error);
+      }
+    }
+    return PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+    )
+      .then((granted) => {
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          resolve("granted");
+        }
+        return reject("Location Permission denied");
+      })
+      .catch((error) => {
+        console.log("Ask Location permission error: ", error);
+        return reject(error);
+      });
+  });
