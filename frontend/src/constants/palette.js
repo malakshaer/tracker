dc30e4f29071cb576942158a0cc351c38534e32c
malakshaer <shaermk06@gmail.com>
2022-11-16 08:30:12 +0200

    frontend: add the palette that contain colors names

---
+++ frontend/src/constants/palette.js
@@ -0,0 +1,20 @@
+export const colors = {
+  background: "#032955",
+  app_background: "#F5F5F5",
+  primary: "#A47551",
+  primary_dark: "#523A28",
+  red: "#F11B53",
+  green: "#23CB66",
+  gold: "#D2BA23",
+  black: "#000000",
+  white: "#FFFFFF",
+
+  text: "#777777",
+  text_dark: "#323643",
+  disabled_text: "#BCC1C5",
+  border_lines: "#DFDFDF",
+};
+
+export default {
+  colors,
+};
