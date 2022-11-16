f4932dd5af2600b12648da520d45f934290fa9d8
malakshaer <shaermk06@gmail.com>
2022-11-16 08:09:12 +0200

    frontend: call the edit user api using axios

---
+++ frontend/src/api/userApi.js
@@ -0,0 +1,24 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+const BASE_URL = "http://127.0.0.1:8000/api/auth";
+
+export const editUser = async (name, email, password) => {
+  const user = JSON.parse((await AsyncStorage.getItem("user")) || "");
+  console.log("data");
+
+  const res = await axios.put(
+    `${BASE_URL}/editUser`,
+    {
+      name,
+      email,
+      password,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${user?.access_token}`,
+      },
+    }
+  );
+
+  return res;
+};
