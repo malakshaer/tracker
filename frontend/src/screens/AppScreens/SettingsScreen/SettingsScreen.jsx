0eb3216027ff676c60fcd1e2309903a034f99719
malakshaer <shaermk06@gmail.com>
2022-11-16 07:19:30 +0200

    frontend: addthe styles of the screen

--- frontend/src/screens/AppScreens/SettingsScreen/SettingsScreen.jsx
+++ frontend/src/screens/AppScreens/SettingsScreen/SettingsScreen.jsx
@@ -138,4 +138,36 @@ const SettingsScreen = ({ navigation }) => {
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+  popUpContainer: {
+    alignItems: "center",
+  },
+  header: {
+    width: "100%",
+    height: 40,
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row",
+  },
+  textPopUp: {
+    marginVertical: 30,
+    fontSize: 20,
+    textAlign: "center",
+    color: "#032955",
+  },
+  option: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textPermission: {
+    marginVertical: 20,
+    fontSize: 20,
+    color: "#032955",
+    paddingRight: 50,
+  },
+});
+
 export default SettingsScreen;
