8d8e0bb09b7298beaadc29aae73ac6b276e5a775
malakshaer <shaermk06@gmail.com>
2022-11-17 19:44:01 +0200

    frontend: call login api

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,11 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+const BASE_URL = "https://127.0.0.1:8000/api/auth";
+
+export const login = async (email, password) => {
+  const res = axios.post(`${BASE_URL}/login`, {
+    email,
+    password,
+  });
+  return res;
+};
