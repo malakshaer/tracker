c54ded5c3e6ea3a38126def321ecf7266c255b5b
malakshaer <shaermk06@gmail.com>
2022-11-18 12:54:07 +0200

    frontend: add password confirmation to register function

--- frontend/src/api/authApi.tsx
+++ frontend/src/api/authApi.tsx
@@ -20,13 +20,13 @@ export const register = async (
   name,
   email,
   password,
-  // password_confirmation
+  password_confirmation
 ) => {
   const res = axios.post(`http://10.0.2.2:8000/api/auth/register`, {
     name,
     email,
     password,
-    // password_confirmation,
+    password_confirmation,
   });
   return res;
 };
