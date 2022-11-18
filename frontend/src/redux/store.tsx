b0e521c01d306b0b245cfba7b23b03d3c6b11286
malakshaer <shaermk06@gmail.com>
2022-11-18 15:55:13 +0200

    frontend: add car reducer

--- frontend/src/redux/store.tsx
+++ frontend/src/redux/store.tsx
@@ -1,6 +1,7 @@
 import { configureStore  } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import userReducer from "./slices/userSlice";
+import carReducer from "./slices/carSlice";
 
 
 
@@ -8,6 +9,7 @@ export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    car: carReducer,
   },
   
 });
