aac9ed4e8465f9096f2c8675c5a189684d8a6d6e
malakshaer <shaermk06@gmail.com>
2022-11-18 16:48:18 +0200

    frontend: modify base url

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "react-native";
 
-const BASE_URL = "http://127.0.0.1:8000/api/auth";
+const BASE_URL = "http://10.0.2.2:8000/api/auth";
 
 export const authApi = axios.create({
   baseURL: BASE_URL,
