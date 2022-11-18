ca9df4a7a6ad6b1e564ec142aeabca2c25a6d553
malakshaer <shaermk06@gmail.com>
2022-11-18 10:24:40 +0200

    frontend: change file to typescript

---
+++ frontend/src/navigation/StackSwitcher.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import  TabStack  from "./TabStack";
+import { OnboardingStack } from "./OnboardingStack";
+
+export function StackSwitcher() {
+  const user = useSelector((state) => state?.user);
+
+  // for bottom tabs navigation
+  return user?.userProfile ? <TabStack /> : <OnboardingStack />;
+}
