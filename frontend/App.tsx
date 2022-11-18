1a2ed82f5ede9da87e04562fa8493ce39ddb7107
malakshaer <shaermk06@gmail.com>
2022-11-18 17:44:48 +0200

    frontend: clean the code

---
+++ frontend/App.tsx
@@ -0,0 +1,14 @@
+import "react-native-gesture-handler";
+import { Provider } from "react-redux";
+import { StackSwitcher } from "./src/navigation/StackSwitcher";
+import { store } from "./src/redux/store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <StackSwitcher />
+    </Provider>
+  );
+};
+
+export default App;
