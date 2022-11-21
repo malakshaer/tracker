70dfa1db764eedfa22064c6820a60dccd457f93a
malakshaer <shaermk06@gmail.com>
2022-11-21 07:58:23 +0200

    frontend: import styles from profile screen styles

--- frontend/src/screens/AppScreens/ProfileScreen/ProfileScreen.jsx
+++ frontend/src/screens/AppScreens/ProfileScreen/ProfileScreen.jsx
@@ -14,6 +14,7 @@ import {
   ScrollView,
 } from "react-native";
 import userProfile from "../../../../assets/user.jpg";
+import styles from "./ProfileScreenStyles";
 import Icon from "react-native-vector-icons/Ionicons";
 import CarComponent from "../../../components/CarComponent/CarComponent";
 import ModalPopup from "../../../components/Modal/Modal";
@@ -57,9 +58,6 @@ const ProfileScreen = ({ props }) => {
 
   const [carName, setCarName] = useState();
   const [pin, setPin] = useState();
-
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -332,24 +330,6 @@ const ProfileScreen = ({ props }) => {
               <TextInput style={styles.textPopUp} placeholder="PIN"></TextInput>
               <Icon name={"pencil-sharp"} size={30} color={"#032955"} />
             </View>
-            {/* <View style={styles.default}>
-            <TouchableOpacity>
-              <Icon
-                name={iconName}
-                size={30}
-                color={"#032955"}
-                onPress={() => {
-                  if (iconName == "checkmark-circle-outline") {
-                    setIconName("checkmark-circle");
-                  }
-                  if (iconName == "checkmark-circle") {
-                    setIconName("checkmark-circle-outline");
-                  }
-                }}
-              />
-            </TouchableOpacity>
-            <Text>Make this my default vehicle</Text>
-          </View> */}
 
             <TouchableOpacity
               activeOpacity={0.5}
@@ -372,105 +352,4 @@ const ProfileScreen = ({ props }) => {
   }
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-  },
-  containerOne: {
-    flexDirection: "row",
-    paddingTop: 20,
-    paddingLeft: 15,
-    alignItems: "center",
-  },
-  userName: {
-    color: "#032955",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  containerTwo: {
-    flexDirection: "row",
-    paddingLeft: 45,
-    marginVertical: 15,
-  },
-  image: {
-    borderRadius: 70,
-    width: 100,
-    height: 100,
-  },
-  editButton: {
-    elevation: 8,
-    backgroundColor: "#1648AD",
-    borderRadius: 50,
-    paddingVertical: 10,
-    marginHorizontal: 80,
-    paddingHorizontal: 20,
-  },
-  buttonText: {
-    fontSize: 18,
-    color: "#fff",
-    fontWeight: "bold",
-    alignSelf: "center",
-  },
-  headerSendNotification: {
-    width: "100%",
-    height: 40,
-    alignItems: "flex-end",
-  },
-  imageProfile: {
-    alignItems: "center",
-    marginBottom: 30,
-  },
-  option: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: 10,
-    paddingVertical: 20,
-    marginHorizontal: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#1648AD",
-  },
-  appButtonContainer: {
-    elevation: 8,
-    backgroundColor: "#1648AD",
-    borderRadius: 50,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    marginVertical: 20,
-  },
-  deleteCarButton: {
-    elevation: 8,
-    backgroundColor: "red",
-    borderRadius: 50,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-  },
-  appButtonText: {
-    fontSize: 18,
-    color: "#fff",
-    fontWeight: "bold",
-    alignSelf: "center",
-    paddingHorizontal: 20,
-  },
-  addButton: {
-    position: "absolute",
-    marginTop: 530,
-    marginLeft: 320,
-    bottom: 10,
-    right: 20,
-  },
-  titleVehicle: {
-    marginLeft: 20,
-    marginVertical: 10,
-    fontSize: 18,
-    color: "#032955",
-    fontWeight: "bold",
-  },
-  default: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-  },
-});
 export default ProfileScreen;
