112c0560220d57b66c8b2b1fa9e95b4c2ac93a49
malakshaer <shaermk06@gmail.com>
2022-11-21 08:22:30 +0200

    frontend: import styles from settings styles file

--- frontend/src/screens/AppScreens/SettingsScreen/SettingsScreen.tsx
+++ frontend/src/screens/AppScreens/SettingsScreen/SettingsScreen.tsx
@@ -9,6 +9,7 @@ import { deleteAllNotifications } from "../../../api/notificationsApi";
 import { deleteAllCars } from "../../../api/carApi";
 import { deleteAccount } from "../../../api/userApi";
 import { deleteUser } from "../../../redux/slices/userSlice";
+import styles from "./SettingsScreenStyles";
 
 const SettingsScreen = ({ navigation }) => {
   const [visible, setVisible] = React.useState(false);
@@ -138,37 +139,5 @@ const SettingsScreen = ({ navigation }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    marginVertical: 20,
-  },
-  popUpContainer: {
-    alignItems: "center",
-  },
-  header: {
-    width: "100%",
-    height: 40,
-    alignItems: "center",
-    justifyContent: "space-around",
-    flexDirection: "row",
-  },
-  textPopUp: {
-    marginVertical: 30,
-    fontSize: 20,
-    textAlign: "center",
-    color: "#032955",
-  },
-  option: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  textPermission: {
-    marginVertical: 20,
-    fontSize: 20,
-    color: "#032955",
-    paddingRight: 50,
-  },
-});
 
 export default SettingsScreen;
