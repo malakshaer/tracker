16a21017c9f2b711d0f62b28a25953a939471df6
malakshaer <shaermk06@gmail.com>
2022-11-21 12:51:55 +0200

    frontend: modify get location function

--- frontend/src/screens/AppScreens/MapScreen/MapScreen.tsx
+++ frontend/src/screens/AppScreens/MapScreen/MapScreen.tsx
@@ -7,6 +7,7 @@ import {
   Dimensions,
   Image,
   Platform,
+  SafeAreaView,
 } from "react-native";
 import MapView, { Marker, Callout, AnimatedRegion } from "react-native-maps";
 import { GOOGLE_MAP_KEY } from "../../../constants/googleMapKey";
@@ -20,136 +21,68 @@ import sendNotification from "../NotificationScreen/NotificationScreen";
 import locate from "../../../../assets/locate.png";
 import * as Location from "expo-location";
 import styles from "./MapScreenStyles";
+import * as geoLocation from "expo-location";
+import Constants from "expo-constants";
+import Loading from "../../../components/Loading/Loading";
 
-const screen = Dimensions.get("window");
-const ASPECT_RATIO = screen.width / screen.height;
-const LATITUDE_DELTA = 0.04;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+// const screen = Dimensions.get("window");
+// const ASPECT_RATIO = screen.width / screen.height;
+// const LATITUDE_DELTA = 0.04;
+// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 const MapScreen = () => {
-  const mapRef = useRef();
-  const markerRef = useRef();
-
-  const [location, setLocation] = React.useState(null);
-  const [errorMsg, setErrorMsg] = React.useState(null);
-
-  const [state, setState] = useState({
-    currentLocation: {
-      latitude: 33.88863,
-      longitude: 35.49548,
-    },
-    destinationCords: {},
-    isLoading: false,
-    coordinate: new AnimatedRegion({
-      latitude: 33.88863,
-      longitude: 35.49548,
-      latitudeDelta: LATITUDE_DELTA,
-      longitudeDelta: LONGITUDE_DELTA,
-    }),
-    time: 0,
-    distance: 0,
-    heading: 0,
+  const [load, setLoad] = useState(false);
+  const [myLocation, setMyLocation] = useState({
+    latitude: 33.450736,
+    longitude: 35.396315,
   });
 
-  const {
-    currentLocation,
-    time,
-    distance,
-    destinationCords,
-    isLoading,
-    coordinate,
-    heading,
-  } = state;
-  const updateState = (data) => setState((state) => ({ ...state, ...data }));
-
   useEffect(() => {
-    getLiveLocation();
-  }, []);
-
-  //get the live location of the vehicle
-  const getLiveLocation = async () => {
-    const locPermissionDenied = await locationPermission();
-    if (locPermissionDenied) {
-      const { latitude, longitude, heading } = await getCurrentLocation();
-      console.log("get live location after 4 second", heading);
-      animate(latitude, longitude);
-      updateState({
-        heading: heading,
-        currentLocation: { latitude, longitude },
-        coordinate: new AnimatedRegion({
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA,
-        }),
-      });
-    }
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      getLiveLocation();
-    }, 20000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const animate = (latitude, longitude) => {
-    const newCoordinate = { latitude, longitude };
-    if (Platform.OS == "android") {
-      if (markerRef.current) {
-        markerRef.current.animateMarkerToCoordinate(newCoordinate, 7000);
-      }
-    } else {
-      coordinate.timing(newCoordinate).start();
-    }
-  };
-
-  //locate the car on the screen
-  const onCenter = () => {
-    mapRef.current.animateToRegion({
-      latitude: currentLocation.latitude,
-      longitude: currentLocation.longitude,
-      latitudeDelta: LATITUDE_DELTA,
-      longitudeDelta: LONGITUDE_DELTA,
-    });
-  };
-
-  //get the distance and time
-  const fetchTime = (d, t) => {
-    updateState({
-      distance: d,
-      time: t,
-    });
-  };
-
-  //get permission to
-  React.useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
+    setLoad(true);
+    const getLocation = async () => {
+      let { status } = await geoLocation.requestForegroundPermissionsAsync();
       if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
+        alert("Permission to access location was denied");
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
+      let getLocation = await geoLocation.getCurrentPositionAsync({});
+      //  console.log("my ", getLocation);
+      setMyLocation({
+        latitude: getLocation.coords.latitude,
+        longitude: getLocation.coords.longitude,
+      });
+      setLoad(false);
+    };
+    getLocation();
   }, []);
 
+  if (load) {
+    return <Loading />;
+  }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={{ flex: 1 }}>
-        <MapView
-          ref={mapRef}
-          style={StyleSheet.absoluteFill}
-          initialRegion={{
-            ...currentLocation,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-          }}
-          provider={"google"}
-        >
-          <Marker.Animated ref={markerRef} coordinate={coordinate}>
+      <MapView
+        style={styles.mapView}
+        initialRegion={{
+          latitude: myLocation.latitude,
+          longitude: myLocation.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+        onUserLocationChange={(e) => {
+          //   console.log("e", e.nativeEvent.coordinate);
+          // setMyLocation({
+          //   longitude: e.nativeEvent.longitude,
+          //   latitude: e.nativeEvent.latitude,
+          // });
+        }}
+      >
+        {/* <Marker coordinate={myLocation} title={"Your car is here"} /> */}
+        <Marker.Animated coordinate={myLocation}>
             <Image
               source={logoMarker}
               style={{
@@ -163,33 +96,24 @@ const MapScreen = () => {
             </Callout>
           </Marker.Animated>
 
-          {Object.keys(destinationCords).length > 0 && (
-            <MapViewDirections
-              origin={currentLocation}
-              destination={destinationCords}
-              apikey={GOOGLE_MAP_KEY}
-              strokeWidth={6}
-              strokeColor="red"
-              optimizeWaypoints={true}
-              onStart={() => {
-                //push notification
-                sendNotification();
-                //send notification to the server
-                sendNewNotification();
-                console.log(`Started routing"`);
-              }}
-              onReady={(result) => {
-                console.log(`Distance: ${result.distance} km`);
-                console.log(`Duration: ${result.duration} min.`);
-                fetchTime(result.distance, result.duration),
-                  mapRef.current.fitToCoordinates(result.coordinates);
-              }}
-              onError={(error) => {
-                // console.log(error);
-              }}
-            />
-          )}
-        </MapView>
+        <MapViewDirections
+          origin={{
+            latitude: 33.88863,
+            longitude: 35.49548,
+          }}
+          destination={myLocation}
+          apikey={GOOGLE_MAP_KEY}
+          strokeWidth={3}
+          // strokeColor={colors.darker}
+        />
+        <Marker
+          coordinate={{
+            latitude: 33.88863,
+            longitude: 35.49548,
+          }}
+          title={"Picker Location"}
+        />
+      </MapView>
 
         <TouchableOpacity
           style={{
@@ -197,7 +121,7 @@ const MapScreen = () => {
             bottom: 10,
             right: 10,
           }}
-          onPress={onCenter}
+          // onPress={onCenter}
         >
           <Image
             source={locate}
@@ -208,8 +132,8 @@ const MapScreen = () => {
           />
         </TouchableOpacity>
       </View>
-      <Loader isLoading={isLoading} />
-    </View>
+      {/* <Loader isLoading={isLoading} /> */}
+    </SafeAreaView>
   );
 };
 
