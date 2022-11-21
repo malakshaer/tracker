378338bc3c1886330f141dc591ee6a5bae18e919
malakshaer <shaermk06@gmail.com>
2022-11-21 08:09:03 +0200

    frontend: add styles of the map screen

---
+++ frontend/src/screens/AppScreens/MapScreen/MapScreenStyles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bottomCard: {
+    backgroundColor: "white",
+    width: "100%",
+    padding: 30,
+    borderTopEndRadius: 24,
+    borderTopStartRadius: 24,
+  },
+  inputStyle: {
+    backgroundColor: "white",
+    borderRadius: 4,
+    borderWidth: 1,
+    alignItems: "center",
+    height: 48,
+    justifyContent: "center",
+    marginTop: 16,
+  },
+});
+
+export default styles;
