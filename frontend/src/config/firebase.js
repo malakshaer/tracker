d78b4082f0a251f964397155e6495fa8c078fb74
malakshaer <shaermk06@gmail.com>
2022-11-23 07:51:20 +0200

    frontend: add get token function for messages

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,7 +1,5 @@
-import * as firebase from "firebase";
-// import firestore from "firebase/firestore";
-// import initializeApp from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getMessaging, onMessage, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyArtd9N_jp8qlojUxUSMvFst2zKhr1Vt_c",
@@ -14,20 +12,33 @@ const firebaseConfig = {
   measurementId: "G-CCQNTFRVJJ",
 };
 
-// firebase.initializeApp(firebaseConfig);
-// firebase.firestore();
-// export default firebase;
+initializeApp(firebaseConfig);
 
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-// export default db;
+const firebase = initializeApp(firebaseConfig);
+const messaging = getMessaging(firebase);
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-firebase
-  .firestore()
-  .settings({ experimentalForceLongPolling: true, merge: true });
-
-export { firebase };
+export const getTokens = async (setTokenFound) => {
+  return getToken(messaging, {
+    vapidKey:
+      "BLcOxljpHN1ZdxCen2yvaVA_mD_kXHEQGcVuDvDBaUdTzjwW_DqAiFQWYRFkgGzp7A_JkYQVvv7THi3Ze_7dD-8",
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        localStorage.setItem("device_token", currentToken);
+        console.log(currentToken);
+        setTokenFound(true);
+      } else {
+        console.log("No registration token available");
+        setTokenFound(false);
+      }
+    })
+    .catch((err) => {
+      console.log("An error occurred while retrieving token. ", err);
+    });
+};
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      resolve(payload);
+    });
+  });
