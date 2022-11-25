06b65efdf04c0e8b52fdfc65e633e7e77f1b3b8b
malakshaer <shaermk06@gmail.com>
2022-11-25 15:42:22 +0200

    admin: add the side bar component

---
+++ Admin/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,78 @@
+import MaterialIcon from "material-icons-react";
+import React, { useLayoutEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./SideBar.css";
+
+const SideBar = () => {
+  const [activeStats, setActiveStats] = useState("inactive");
+
+  useLayoutEffect(() => {
+    if (window.location.pathname === "/stats") {
+      setActiveStats("active");
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="sidebar">
+        <div className="logo-container">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="sidebar-menu">
+          {/* <Link
+            to={"/home"}
+            onClick={() => {
+              setActiveStats("inactive");
+            }}
+          >
+            <div
+              className={`sidebar-menu-item
+            ${activeHome}
+            `}
+            > */}
+          {/* <div className="sidebar-menu-item-icon">
+                <MaterialIcon icon="home" size={"large"} color="#ffffff" />
+              </div> */}
+          {/* <div
+                className={`sidebar-menu-item-text ${activeHome}
+            `}
+              >
+                Home
+              </div> */}
+          {/* </div>
+          </Link> */}
+
+          <Link
+            to={"/stats"}
+            onClick={() => {
+              setActiveStats("active");
+            }}
+          >
+            <div className={`sidebar-menu-item ${activeStats}`}>
+              <div className="sidebar-menu-item-icon">
+                <MaterialIcon icon="bar_chart" size={"large"} color="#ffffff" />
+              </div>
+              <div
+                className={`
+                sidebar-menu-item-text ${activeStats}
+              `}
+              >
+                Stats
+              </div>
+            </div>
+          </Link>
+
+          <Link
+            to={"access"}
+            onClick={() => {
+              setActiveStats("inactive");
+            }}
+          ></Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
