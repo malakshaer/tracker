435eb49e27ebb4e37a1e24a6a1464685598a160d
malakshaer <shaermk06@gmail.com>
2022-11-25 16:42:59 +0200

    frontend: and user image to use context

--- frontend/src/redux/userContext.tsx
+++ frontend/src/redux/userContext.tsx
@@ -5,12 +5,18 @@ export const UserContext = createContext();
 const UserProvider = ({ children }) => {
 
     const [user, setUser] = useState([]);
+    const [cars, setCars] = useState([]);
+    const [userImage, setUserImage] = useState();
 
     return (
         <UserContext.Provider
             value={{
                 user,
                 setUser,
+                cars,
+                setCars,
+                userImage,
+                setUserImage,
             }}
         >
             {children}
