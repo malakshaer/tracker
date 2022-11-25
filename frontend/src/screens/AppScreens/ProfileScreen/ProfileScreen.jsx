29841292bc0c541318ccb4813c47bf0a0b184270
malakshaer <shaermk06@gmail.com>
2022-11-25 19:22:46 +0200

    frontend: add upload image

--- frontend/src/screens/AppScreens/ProfileScreen/ProfileScreen.jsx
+++ frontend/src/screens/AppScreens/ProfileScreen/ProfileScreen.jsx
@@ -1,10 +1,4 @@
-import React, {
-  useCallback,
-  useReducer,
-  useState,
-  useContext,
-  useEffect,
-} from "react";
+import React, { useState, useContext, useEffect } from "react";
 import {
   View,
   Text,
@@ -12,179 +6,251 @@ import {
   StyleSheet,
   TouchableOpacity,
   ScrollView,
+  FlatList,
+  Pressable,
 } from "react-native";
-import userProfile from "../../../../assets/user.jpg";
 import styles from "./ProfileScreenStyles";
 import Icon from "react-native-vector-icons/Ionicons";
 import CarComponent from "../../../components/CarComponent/CarComponent";
 import ModalPopup from "../../../components/Modal/Modal";
 import { TextInput } from "react-native-gesture-handler";
-
-import { editCar } from "../../../api/carFirebase";
-import { deleteCar } from "../../../api/carFirebase";
-
-import add from "../../../../assets/add.png";
-import set from "../../../redux/slices/userSlice";
-import Loading from "../../../components/Loading/Loading";
-import { profile } from "../../../api/authApi";
-import { createNewCar } from "../../../api/carFirebase";
-import { editUser } from "../../../api/userApi";
-import { getAllCars } from "../../../api/carFirebase";
-import firestore from "@react-native-firebase/firestore";
+import { createNewCar } from "../../../api/carApi";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import { UserContext } from "../../../redux/userContext";
+import { addCar } from "../../../../assets/add.png";
+import { deleteCar } from "../../../api/carApi";
+import * as ImagePicker from "expo-image-picker";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const ProfileScreen = ({ props }) => {
+const ProfileScreen = () => {
   const [visibleEditProfile, setVisibleEditProfile] = useState(false);
   const [visibleAddVehicle, setVisibleAddVehicle] = useState(false);
   const [visibleEditVehicle, setVisibleEditVehicle] = useState(false);
 
-  // const [iconName, setIconName] = React.useState("checkmark-circle-outline");
+  const { user, setUser, profileImage } = useContext(UserContext);
 
-  const [cars, setCars] = useState([
-    {
-      carName: "BMW",
-      pin: "1234KIA54321",
-    },
-    {
-      carName: "TOYOTA COROLLA",
-      pin: "1234KIA54321",
-    },
-    {
-      carName: "MERCEDES",
-      pin: "1234KIA54321",
-    },
-  ]);
-
-  const [carName, setCarName] = useState();
-  const [pin, setPin] = useState();
-
+  const [userImage, setUserImage] = useState(profileImage);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [add, setAdd] = useState(false);
+
+  const [carName, setCarName] = useState("");
+  const [pin, setPin] = useState("");
+
+  const [pressedCar, setPressedCar] = useState({});
 
   const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState([]);
 
-  const handleEditUser = useCallback(async () => {
-    try {
-      setLoading(true);
+  const uploadImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
-      const res = await editUser(name, email, password);
+    console.log(result);
 
-      props.navigation.goBack();
-      return res;
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
+    if (!result.cancelled) {
+      setUserImage(result.uri);
     }
-  }, [name, email, password]);
+  };
+
+  const editCar = async (carName, pin, car_id) => {
+    const data = {
+      carName,
+      pin,
+      car_id,
+    };
+    const token = await AsyncStorage.getItem("@token");
+
+    await axios({
+      headers: { Authorization: `Bearer  ${token}` },
+      method: "PUT",
+      url: `${BASE_URL}editCar`,
+      data: data,
+    }).catch(function (error) {
+      console.log(error);
+      return false;
+    });
+  };
+
+  const handleEditUser = async () => {
+    const data = {
+      name,
+      email,
+    };
+    const token = await AsyncStorage.getItem("@token");
+
+    await axios({
+      headers: { Authorization: `Bearer  ${token}` },
+      method: "PUT",
+      url: `${BASE_URL}editUser`,
+      data: data,
+    }).catch(function (error) {
+      console.log(error);
+      return false;
+    });
+  };
 
   const getProfile = async () => {
     const token = await AsyncStorage.getItem("@token");
 
-    const config = {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-      url: `http://10.0.2.2:8000/api/auth/showProfile`,
-    };
-    try {
-      const res = await axios(config);
-      if (res.data.status == "success") {
-        setUser(res.data.data);
-      }
-    } catch (error) {
-      console.warn(error.response.data);
-      return error;
-    }
+    setUser(JSON.parse(await AsyncStorage.getItem("@user")));
   };
 
   useEffect(() => {
     getProfile();
   });
 
-  // useEffect(() => {
-  //   const carList = firestore()
-  //     .collection("cars")
-  //     .onSnapshot((querySnapshot) => {
-  //       const cars = querySnapshot.docs.map((documentSnapshot) => {
-  //         return {
-  //           _id: documentSnapshot.id,
-  //           carName: "",
-  //           pin: "",
-  //           ...documentSnapshot.data(),
-  //         };
-  //       });
+  const [cars, setCars] = useState([]);
 
-  //       setCars(cars);
+  useEffect(async () => {
+    axios({
+      method: "GET",
+      url: `${BASE_URL}getUserCars`,
+      headers: {
+        Authorization: `Bearer  ${await AsyncStorage.getItem("@token")}`,
+      },
+    }).then((res) => {
+      console.log(res.data);
+      setCars(res.data.cars);
+    });
+  }, []);
 
-  //       if (loading) {
-  //         setLoading(false);
-  //       }
-  //     });
-  //   return () => carList();
-  // }, []);
   if (user) {
     return (
       <View style={styles.container}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={styles.containerOne}>
-            <View style={{ paddingLeft: 40 }}>
-              <Text style={styles.userName}>{user.name}</Text>
+        <View style={styles.containerOne}>
+          {/* <Image
+            source={require("../../../../assets/profile.jpg")}
+            style={{ height: 70, width: 70, borderRadius: 40 }}
+          /> */}
+          <Image
+            source={{ uri: userImage }}
+            style={{ height: 70, width: 70, borderRadius: 40 }}
+          />
+          <View style={{ paddingLeft: 20 }}>
+            <Text style={styles.userName}>{user.name}</Text>
+          </View>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => setVisibleEditProfile(true)}
+            style={styles.editButton}
+          >
+            <Text style={styles.buttonText}>Edit Profile</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerTwo}>
+          <Icon
+            name={"mail"}
+            size={20}
+            color={"#1648AD"}
+            style={{ paddingHorizontal: 10 }}
+          />
+          <Text>{user.email}</Text>
+        </View>
+
+        <View
+          style={{
+            borderWidth: 0.5,
+            borderBottomColor: "#032955",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <View style={styles.vehiclesHeader}>
+          <Text style={styles.yourVehicle}>Your Vehicles:</Text>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => setVisibleAddVehicle(true)}
+          >
+            <Text style={styles.addCarButton}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <ScrollView>
+            {cars.map((c) => {
+              return (
+                <View>
+                  <CarComponent
+                    carName={c.carName}
+                    pin={c.pin}
+                    onPress={() => {
+                      setVisibleEditVehicle(true);
+                      setPressedCar(c);
+                    }}
+                  />
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+
+        <ModalPopup visible={visibleEditVehicle}>
+          <View>
+            <View style={styles.headerSendNotification}>
+              <TouchableOpacity onPress={() => setVisibleEditVehicle(false)}>
+                <Image
+                  source={require("../../../../assets/x.png")}
+                  style={{ height: 20, width: 20 }}
+                />
+              </TouchableOpacity>
             </View>
+            <View style={styles.imageProfile}>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: "#032955",
+                    fontSize: 20,
+                    fontWeight: "bold",
+                  }}
+                >
+                  Edit Your Vehicle
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.option}>
+              <TextInput
+                style={styles.textPopUp}
+                placeholder="Car Name/Type"
+                onChangeText={(value) => {
+                  setCarName(value);
+                }}
+              ></TextInput>
+              <Icon name={"pencil-sharp"} size={30} color={"#032955"} />
+            </View>
+            <View style={styles.option}>
+              <TextInput
+                style={styles.textPopUp}
+                placeholder="PIN"
+                onChangeText={(value) => {
+                  setPin(value);
+                }}
+              ></TextInput>
+              <Icon name={"pencil-sharp"} size={30} color={"#032955"} />
+            </View>
+
             <TouchableOpacity
               activeOpacity={0.5}
-              onPress={() => setVisibleEditProfile(true)}
-              style={styles.editButton}
+              onPress={() => {
+                editCar(carName, pin, pressedCar.id);
+                setVisibleEditVehicle(false);
+              }}
+              style={styles.appButtonContainer}
             >
-              <Text style={styles.buttonText}>Edit Profile</Text>
+              <Text style={styles.appButtonText}>Save</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              activeOpacity={0.5}
+              onPress={() => deleteCar(pressedCar.id)}
+              style={styles.deleteCarButton}
+            >
+              <Text style={styles.appButtonText}>Delete Car</Text>
             </TouchableOpacity>
           </View>
-          <View style={styles.containerTwo}>
-            <Icon
-              name={"mail"}
-              size={20}
-              color={"#1648AD"}
-              style={{ paddingHorizontal: 10 }}
-            />
-            <Text>{user.email}</Text>
-          </View>
-
-          <View
-            style={{
-              borderWidth: 0.5,
-              borderBottomColor: "#032955",
-              borderBottomWidth: StyleSheet.hairlineWidth,
-            }}
-          />
-          <View>
-            {loading ? (
-              <Loading />
-            ) : (
-              cars.map((car) => (
-                <CarComponent
-                  carName={car.carName}
-                  pin={car.pin}
-                  onPress={() => setVisibleEditVehicle(true)}
-                />
-              ))
-            )}
-          </View>
-        </ScrollView>
-
-        <TouchableOpacity
-          style={styles.addButton}
-          onPress={() => setVisibleAddVehicle(true)}
-        >
-          <Image
-            source={add}
-            style={{
-              width: 70,
-              height: 70,
-            }}
-          />
-        </TouchableOpacity>
+        </ModalPopup>
 
         <ModalPopup visible={visibleEditProfile}>
           <View>
@@ -196,38 +262,68 @@ const ProfileScreen = ({ props }) => {
                 />
               </TouchableOpacity>
             </View>
+            <View>
+              {/* <Image
+                source={require("../../../../assets/profile.jpg")}
+                style={{
+                  height: 70,
+                  width: 70,
+                  borderRadius: 40,
+                  alignSelf: "center",
+                }}
+              /> */}
+              <Image
+                source={{ uri: userImage }}
+                style={{
+                  height: 70,
+                  width: 70,
+                  borderRadius: 40,
+                  alignSelf: "center",
+                }}
+              />
+              <TouchableOpacity onPress={uploadImage}>
+                <Text style={styles.uploadImage}>Upload</Text>
+              </TouchableOpacity>
+            </View>
 
             <View style={styles.option}>
               <TextInput
                 style={styles.textPopUp}
-                onChangeText={setName}
+                onChangeText={(value) => {
+                  setName(value);
+                }}
                 placeholder={user.name}
-                defaultValue={user.name}
               ></TextInput>
               <Icon name={"pencil-sharp"} size={30} color={"#032955"} />
             </View>
             <View style={styles.option}>
               <TextInput
                 style={styles.textPopUp}
-                onChangeText={setEmail}
+                onChangeText={(value) => {
+                  setEmail(value);
+                }}
                 placeholder={user.email}
-                defaultValue={user.email}
               ></TextInput>
               <Icon name={"pencil-sharp"} size={30} color={"#032955"} />
             </View>
             <View style={styles.option}>
               <TextInput
                 style={styles.textPopUp}
-                onChangeText={setPassword}
-                placeholder={user.password}
+                onChangeText={(value) => {
+                  setPassword(value);
+                }}
+                placeholder="Edit password"
                 defaultValue={user.password}
               ></TextInput>
               <Icon name={"pencil-sharp"} size={30} color={"#032955"} />
             </View>
             <TouchableOpacity
               activeOpacity={0.5}
-              onPress={() => handleEditUser()}
               style={styles.appButtonContainer}
+              onPress={() => {
+                handleEditUser();
+                setVisibleEditProfile(false);
+              }}
             >
               <Text style={styles.appButtonText}>Save</Text>
             </TouchableOpacity>
@@ -256,94 +352,35 @@ const ProfileScreen = ({ props }) => {
             <View style={styles.option}>
               <TextInput
                 style={styles.textPopUp}
-                onChangeText={setCarName}
-                value={carName}
-                placeholder={"Car Name/Type"}
+                onChangeText={(value) => {
+                  setCarName(value);
+                }}
+                placeholder="Add Car here"
               ></TextInput>
               <Icon name={"add"} size={30} color={"#032955"} />
             </View>
             <View style={styles.option}>
               <TextInput
                 style={styles.textPopUp}
-                onChangeText={setPin}
-                value={pin}
-                placeholder={"PIN"}
+                onChangeText={(value) => {
+                  setPin(value);
+                }}
+                placeholder="Add PIN here"
               ></TextInput>
               <Icon name={"add"} size={30} color={"#032955"} />
             </View>
-            {/* <View style={styles.default}>
-            <TouchableOpacity>
-              <Icon
-                name={iconName}
-                size={30}
-                color={"#032955"}
-                onPress={() => {
-                  if (iconName == "checkmark-circle-outline") {
-                    setIconName("checkmark-circle");
-                  }
-                  if (iconName == "checkmark-circle") {
-                    setIconName("checkmark-circle-outline");
-                  }
-                }}
-              />
-            </TouchableOpacity>
-            <Text>Make this my default vehicle</Text>
-          </View> */}
 
             <TouchableOpacity
               activeOpacity={0.5}
-              onPress={() => createNewCar(carName, pin)}
+              onPress={() => {
+                createNewCar(carName, pin);
+                setVisibleAddVehicle(false);
+                cars.push({ carName: carName, pin: pin });
+                setAdd(!add);
+              }}
               style={styles.appButtonContainer}
             >
               <Text style={styles.appButtonText}>Add</Text>
-            </TouchableOpacity>
-          </View>
-        </ModalPopup>
-
-        <ModalPopup visible={visibleEditVehicle}>
-          <View>
-            <View style={styles.headerSendNotification}>
-              <TouchableOpacity onPress={() => setVisibleEditVehicle(false)}>
-                <Image
-                  source={require("../../../../assets/x.png")}
-                  style={{ height: 20, width: 20 }}
-                />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.imageProfile}>
-              <TouchableOpacity>
-                <Text
-                  style={{ color: "#032955", fontSize: 20, fontWeight: "bold" }}
-                >
-                  Edit Your Vehicle
-                </Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.option}>
-              <TextInput
-                style={styles.textPopUp}
-                placeholder="Car Name/Type"
-              ></TextInput>
-              <Icon name={"pencil-sharp"} size={30} color={"#032955"} />
-            </View>
-            <View style={styles.option}>
-              <TextInput style={styles.textPopUp} placeholder="PIN"></TextInput>
-              <Icon name={"pencil-sharp"} size={30} color={"#032955"} />
-            </View>
-
-            <TouchableOpacity
-              activeOpacity={0.5}
-              onPress={() => editCar()}
-              style={styles.appButtonContainer}
-            >
-              <Text style={styles.appButtonText}>Save</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.5}
-              onPress={() => deleteCar()}
-              style={styles.deleteCarButton}
-            >
-              <Text style={styles.appButtonText}>Delete Car</Text>
             </TouchableOpacity>
           </View>
         </ModalPopup>
